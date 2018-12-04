76ed4ba3bb0efa2cf5851b9738bd67ba660c829b
Azure <azure@AzuredeMacBook-Pro.local>
2018-12-05 00:05:45 +0800

    Initial commit

---
+++ Demo1/KNN.py
@@ -0,0 +1,16 @@
+'''
+Started on Sep 04.2018
+KNN   K-Nearest Neighbour
+@author:Azure1yu
+1st year learning Python and ML ^^
+'''
+
+
+from numpy import *
+import operator
+
+def createDataSet():
+    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    lables = ['A','A','B','B']
+    return group,lables
+
