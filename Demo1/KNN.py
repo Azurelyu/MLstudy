fba50c1578b4abcd2ba37f80e42b025e1c4e7170
Azure1yu <1029538915@qq.com>
2018-12-05 22:38:42 +0800

    ...

--- Demo1/KNN.py
+++ Demo1/KNN.py
@@ -20,3 +20,11 @@ def classify0(inX, dataSet, labels, k):
     diffMat = tile(inX, (dataSetSize,1)) - dataSet
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range (k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1),reverse=True)
+    return sortedClassCount [0] [0]
