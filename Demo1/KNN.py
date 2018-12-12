8852774468b5af0665b7d99b6b62cce6179281bd
Azurelyu <1029538915@qq.com>
2018-12-12 21:00:01 +0800

    ...

--- Demo1/KNN.py
+++ Demo1/KNN.py
@@ -63,3 +63,12 @@ def datingClassTest():
     m = normMat.shape[0]
     numTestVecs = int(m*hoRatio)
     errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print "the classifier came back with :%d, the real answer is : %d"% (classifierResult,datingLabels[i])
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print "the total error rate is: %f" % (errorCount/float(numTestVecs))
+def classifyPerson():
+    resultList = ['not at all','in small doses','in large doses']
+    percentTats = float(raw_input("percentage of time spent playing video games?"))
+    ffMiles = float(raw_input)
